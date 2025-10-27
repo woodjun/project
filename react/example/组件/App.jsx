884f5b3a9113b53c9acd1049be4eff1f68ad0491
woodjun <2244045904@qq.com>
2025-10-27 18:14:04 +0800

    feat(react): 增加组件和生命周期例子

---
+++ react/example/组件/App.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import Weather from "./State";
+import Person from "./Props";
+import Person2 from "./Props/func";
+import Demo from "./Refs";
+import Controll from './Controll'
+
+export default class App extends Component {
+  state = {
+    p: {
+      name: 'jerry',
+      age: '19',
+      sex: '女'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>State</h1>
+        <Weather></Weather>
+        <hr />
+        <h1>Props</h1>
+        {/* 属性单个传递 */}
+        <Person name="tom" age={18} sex="男"></Person>
+        {/* 运用扩展运算符多个属性传递 */}
+        {/* <Person {...this.state.p}></Person> */}
+        <Person2 name="jerry" sex="女"></Person2>
+        <hr />
+        <h1>Refs</h1>
+        <Demo></Demo>
+        <hr />
+        <h1>受控组件和非受控组件</h1>
+        <Controll></Controll>
+      </div>
+    )
+  }
+};
